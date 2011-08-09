73d7701c3120288be5555bdb85b871764418d845
Sebastiaan Visser <code@fvisser.nl>
2011-08-09 16:19:10 +0200

    Added two utilities to ArrowKleisli.

--- src/Control/Arrow/ArrowKleisli.hs
+++ src/Control/Arrow/ArrowKleisli.hs
@@ -13,13 +13,19 @@ module Control.Arrow.ArrowKleisli where
 import Control.Arrow
 import Control.Category
 import Control.Monad.Trans
-import Prelude hiding ((.), id, const)
+import Prelude hiding ((.), id)
 
 class (Monad m, Arrow (~>)) => ArrowKleisli m (~>) | (~>) -> m where
   arrM :: (a -> m b) -> a ~> b
 
 instance Monad m => ArrowKleisli m (Kleisli m) where
   arrM f = Kleisli f
+
+constM :: ArrowKleisli m (~>) => m b -> a ~> b
+constM a = arrM (const a)
+
+effect :: ArrowKleisli m (~>) => m () -> a ~> a
+effect a = arrM (\b -> a >> return b)
 
 arrIO :: (MonadIO m, ArrowKleisli m (~>)) => (a -> IO b) -> a ~> b
 arrIO f = arrM (liftIO . f)
